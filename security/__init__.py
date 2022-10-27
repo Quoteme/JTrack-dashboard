d69536d07613b51fd6d2118146c4283269dcc9a5
Luca Leon Happel <luca.happel@hhu.de>
2022-10-27 22:50:38 +0200

    move password file to root of server
    needed for python to not crash the server

--- security/__init__.py
+++ security/__init__.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-passwd_file = 'passwd.csv'
+# the passwd.csv file must be stored in ./Jrack-dashboard/security/passwd.csv
+# the content should be like this:
+#
+# user,password,role
+# testaccount,meintollespasswort,master
+
+passwd_file = '/passwd.csv'
 columns = ['user', 'password', 'role']
 auth_pairs = pd.read_csv(passwd_file)
 auth_pairs = auth_pairs.reindex(columns=columns)
